9728ae4c7b504d720b9d1dfb6be38f95e69e2874
Nasser Ali AlZahrani <nassersala@gmail.com>
2014-01-30 10:10:53 +1100

    adds assert_raise functionality

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 default:*.c *.h
 	mkdir -p bin
-	cc -o bin/test_the_tests spec_tests.c spec.c
+	cc -o bin/test_the_tests assert.c except.c spec_tests.c spec.c 
 	./bin/test_the_tests
--- spec_tests.c
+++ spec_tests.c
@@ -1,20 +1,74 @@
 #include <stdio.h>
 #include <assert.h>
-#include "spec.h"
+#include "except.h"
 
+#include "spec.h"
 
 #define run_test(fn_name)\
   printf("%s\n", #fn_name);\
   fn_name();
 
+Except_T SomeException = { "Some Exception" };
+Except_T AnotherException = { "Another Exception" };
+
+enum handled {
+  Not_hanlded = 0,
+  SomeException_handled = 1,
+  Another_handled = 2 };
+
+void assert_raise(Except_T expected, Block block);
+
+int is_same_exception(Except_T e1, Except_T e2) {
+  //they are the same exception if reason pointers are the same
+  return e1.reason == e2.reason;
+}
+
+/*--------------spec_tests--------*/
 void test_that_it_can_pass() {
   describe("some thing", ^{
-      //it("has some property", ^{
-        //});
-      });
+    //it("has some property", ^{
+    //});
+  });
+}
+
+void test_that_it_can_fail() {
+  describe("some thing", ^{
+    it("fails", ^{
+      RAISE(SomeException);
+    });
+  });
 }
 
 int main() {
   run_test(test_that_it_can_pass);
+  run_test(test_that_it_can_fail);
+
+  puts("\nOK");
   return 0;
+
+}
+
+void assert_raise(Except_T expected, Block block) {
+  int the_handled = Not_hanlded;
+
+  TRY
+    block();
+  EXCEPT(SomeException)
+    puts("Hanling SomeException exception");
+    the_handled = SomeException_handled;
+  EXCEPT(AnotherException)
+    puts("Hanling AnotherException");
+    the_handled = Another_handled;
+  END_TRY;
+
+  if(is_same_exception(expected, SomeException) && the_handled == SomeException_handled) {
+    puts("SomeException raised and handled");
+    return;
+  }
+  else if(is_same_exception(expected, AnotherException) && the_handled == Another_handled) {
+    puts("AnotherException raised and handled");
+    return;
+  }
+  
+  assert(0 && "didnt raise the expected exception");
 }
